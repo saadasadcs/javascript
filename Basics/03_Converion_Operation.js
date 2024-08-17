da6a84a569bc72262aa2f930fd3fec98aeb6c123
M Saad Asad <saadasadcs@gmail.com>
2024-08-17 10:42:15 +0000

    Conversion Operation

---
+++ Basics/03_Converion_Operation.js
@@ -0,0 +1,144 @@
+let score = "Saad"
+
+// console.log(typeof score);
+
+// console.log(typeof(score));
+
+
+
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+
+
+
+/*
+
+NOTES : 
+
+CONVERSION
+
+    "33" => 33
+    
+    "33abc" => NaN
+    
+    true => 1; 
+    
+    false => 0
+
+    1 => true; 0 => false
+    
+    "" => false
+    
+    "SAAD" => true
+*/
+
+
+
+let isLoggedIn = "SAAD"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+//  console.log(booleanIsLoggedIn);
+
+
+
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+// console.log(stringNumber);
+
+// console.log(typeof stringNumber);
+
+
+
+
+
+/* *********************** Operations *********************** */
+
+let value = 3
+
+let negValue = -value
+
+// console.log(negValue);
+
+
+
+
+
+// BASICS Arithmetic Operators
+
+// console.log(2+2);
+
+// console.log(2-2);
+
+// console.log(2*2);
+
+// console.log(2**3);
+
+// console.log(2/3);
+
+// console.log(2%3);
+
+
+
+
+
+
+// let str1 = "HELLO"
+
+// let str2 = " SAAD"
+
+// let str3 = str1 + str2
+
+// console.log(str3);
+
+
+
+
+
+// console.log("1" + 2);
+
+// console.log(1 + "2");
+
+// console.log("1" + 2 + 2);
+
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+
+// console.log(+"");
+
+
+
+
+
+// let a, b, c;
+
+// a = b = c = 5 * 3;
+
+// console.log(a);
+
+
+
+
+
+// let gameCounter = 100
+
+// ++gameCounter;
+
+// console.log(gameCounter);
+
+
+
+
+// // link to study
+// // https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
