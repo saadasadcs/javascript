f747028b9f113f97ae67a3df96d166a24b3ebe1a
M Saad Asad <saadasadcs@gmail.com>
2024-08-17 14:01:48 +0000

    Data Types

--- Basics/02_datatypes.js
+++ Basics/02_datatypes.js
@@ -21,5 +21,58 @@ let state;
 
 // object
 
-console.log(typeof undefined); // undefined
-console.log(typeof null); // object
+// console.log(typeof undefined); // undefined
+// console.log(typeof null); // object
+
+
+
+
+/*
+
+DETAIL DATA TYPES 
+
+*/
+
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+
+const scoreValue = 100.3
+
+const loggedIn = false
+
+const outsideTemp = null
+
+let userEmail;
+
+const id = Symbol('123')
+
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+console.log(typeof anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["ironman", "flash", "wonderwoman"];
+
+let myObj = {
+    name: "Saad",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
