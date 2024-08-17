1d193a974e4c96fe9ac0aa85f4a925ae4f78ada9
M Saad Asad <saadasadcs@gmail.com>
2024-08-17 10:41:48 +0000

    DataTypes

---
+++ Basics/02_datatypes.js
@@ -0,0 +1,25 @@
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("saad")
+
+
+let name = "saad"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
